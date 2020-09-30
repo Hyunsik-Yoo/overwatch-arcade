889a1ccce32a5af908c8ccacef2534cf227ad91a
Yoo Hyun Sik <dbgustlr92@gmail.com>
2020-10-01 00:09:13 +0900

    화면 상단 아이콘 opacity 애니메이션 추가

--- overwatch-arcade/features/main/MainView.swift
+++ overwatch-arcade/features/main/MainView.swift
@@ -7,81 +7,87 @@ struct MainView: View {
   @ObservedObject var viewModel = MainViewModel(overwatchService: OverwatchService())
   
   var body: some View {
-    ZStack {
-      Image("img_background")
-        .resizable()
-        .edgesIgnoringSafeArea([.top, .bottom])
-      
-      ScrollView(.vertical, showsIndicators: false) {
-        VStack(alignment: .leading, spacing: 0) {
-          HStack {
-            Spacer()
-            
-            Button(action: self.viewModel.fetchArcade) {
-              Image("ic_history")
+    NavigationView {
+      ZStack {
+        Image("img_background")
+          .resizable()
+          .edgesIgnoringSafeArea([.top, .bottom])
+        
+        ScrollView(.vertical, showsIndicators: false) {
+          VStack(alignment: .leading, spacing: 0) {
+            HStack {
+              Spacer()
+              
+              NavigationLink(
+                destination: Text("Destination"),
+                label: {
+                  Image("ic_history")
+                })
+                .padding(.top, 16 * RatioUtils.heightRatio)
             }
-            .padding(.top, 16 * RatioUtils.heightRatio)
-          }
-          
-          Image("img_mayhem")
-            .resizable()
-            .frame(
-              width: 108 * RatioUtils.widthRatio,
-              height: 108 * RatioUtils.widthRatio
-            )
-            .padding(.top, 64 * RatioUtils.heightRatio)
-          
-          Text("2020. 09.30")
-            .font(.custom("koverwatch", size: 20 * RatioUtils.widthRatio))
-            .foregroundColor(.white)
-            .padding(.top, 16 * RatioUtils.heightRatio)
-          
-          Text(self.viewModel.isTodayTotalMayhem ? "오늘은 난장판입니다!\n함께 즐겨요!" : "오늘은 난장판이 아닙니다..\n다음 기회에..")
-            .font(.custom("koverwatch", size: 36 * RatioUtils.widthRatio))
-            .foregroundColor(.white)
-            .padding(.top, 16 * RatioUtils.heightRatio)
-            .lineSpacing(10)
-          
-          MayhemRatioView(predictedValue: self.$viewModel.predictedValue)
-            .padding(.top, 24)
-          
-          HStack(spacing:0) {
-            Text("OVERWATCH ARCADE TODAY")
+            
+            Image("img_mayhem")
+              .resizable()
+              .opacity(self.viewModel.iconOpacity)
+              .frame(
+                width: 108 * RatioUtils.widthRatio,
+                height: 108 * RatioUtils.widthRatio
+              )
+              .padding(.top, 64 * RatioUtils.heightRatio)
+            
+            Text("2020. 09.30")
               .font(.custom("koverwatch", size: 20 * RatioUtils.widthRatio))
               .foregroundColor(.white)
+              .padding(.top, 16 * RatioUtils.heightRatio)
             
-            Spacer()
-            
-            Text("18:42:21")
-              .font(.custom("koverwatch", size: 20 * RatioUtils.widthRatio))
+            Text(self.viewModel.titleText)
+              .font(.custom("koverwatch", size: 36 * RatioUtils.widthRatio))
               .foregroundColor(.white)
+              .padding(.top, 16 * RatioUtils.heightRatio)
+              .lineSpacing(10)
+            
+            MayhemRatioView(predictedValue: self.$viewModel.predictedValue)
+              .padding(.top, 24)
+            
+            HStack(spacing:0) {
+              Text("OVERWATCH ARCADE TODAY")
+                .font(.custom("koverwatch", size: 20 * RatioUtils.widthRatio))
+                .foregroundColor(.white)
+              
+              Spacer()
+              
+              Text("18:42:21")
+                .font(.custom("koverwatch", size: 20 * RatioUtils.widthRatio))
+                .foregroundColor(.white)
+            }
+            .padding(.top, 72 * RatioUtils.heightRatio)
+            
+            BigModeCell(mode: self.$viewModel.mode1)
+              .padding(.top, 8 * RatioUtils.heightRatio)
+            
+            HStack(spacing: 15 * RatioUtils.widthRatio) {
+              ModeCell(mode: self.$viewModel.mode2)
+              ModeCell(mode: self.$viewModel.mode3)
+            }
+            .padding(.top, 24 * RatioUtils.heightRatio)
+            
+            HStack(spacing: 15 * RatioUtils.widthRatio) {
+              ModeCell(mode: self.$viewModel.mode4)
+              ModeCell(mode: self.$viewModel.mode5)
+            }
+            .padding(.top, 24 * RatioUtils.heightRatio)
+            
+            HStack(spacing: 15 * RatioUtils.widthRatio) {
+              ModeCell(mode: self.$viewModel.mode6)
+              ModeCell(mode: self.$viewModel.mode7)
+            }
+            .padding(.top, 24 * RatioUtils.heightRatio)
           }
-          .padding(.top, 72 * RatioUtils.heightRatio)
-          
-          BigModeCell(mode: self.$viewModel.mode1)
-            .padding(.top, 8 * RatioUtils.heightRatio)
-          
-          HStack(spacing: 15 * RatioUtils.widthRatio) {
-            ModeCell(mode: self.$viewModel.mode2)
-            ModeCell(mode: self.$viewModel.mode3)
-          }
-          .padding(.top, 24 * RatioUtils.heightRatio)
-          
-          HStack(spacing: 15 * RatioUtils.widthRatio) {
-            ModeCell(mode: self.$viewModel.mode4)
-            ModeCell(mode: self.$viewModel.mode5)
-          }
-          .padding(.top, 24 * RatioUtils.heightRatio)
-          
-          HStack(spacing: 15 * RatioUtils.widthRatio) {
-            ModeCell(mode: self.$viewModel.mode6)
-            ModeCell(mode: self.$viewModel.mode7)
-          }
-          .padding(.top, 24 * RatioUtils.heightRatio)
+          .padding(.leading, 24 * RatioUtils.widthRatio)
+          .padding(.trailing, 24 * RatioUtils.widthRatio)
         }
-        .padding(.leading, 24 * RatioUtils.widthRatio)
-        .padding(.trailing, 24 * RatioUtils.widthRatio)
       }
+      .navigationBarHidden(true)
     }
     .onAppear(perform: self.viewModel.fetchArcade)
   }
--- overwatch-arcade/features/main/MainViewModel.swift
+++ overwatch-arcade/features/main/MainViewModel.swift
@@ -4,7 +4,8 @@ import SwiftUI
 class MainViewModel: ObservableObject {
   
   @Published var predictedValue = 0
-  @Published var isTodayTotalMayhem = false
+  @Published var iconOpacity = 1.0
+  @Published var titleText = ""
   @Published var mode1: Mode? = nil
   @Published var mode2: Mode? = nil
   @Published var mode3: Mode? = nil
@@ -16,10 +17,21 @@ class MainViewModel: ObservableObject {
   var overwatchService: OverwatchServiceProtocol
   
   var setCancellable = Set<AnyCancellable>()
+  let totalMayhemFilter = PassthroughSubject<Bool, Never>()
   
   
   init(overwatchService: OverwatchServiceProtocol) {
     self.overwatchService = overwatchService
+    self.totalMayhemFilter.sink { isTodayTotalMayhem in
+      if isTodayTotalMayhem {
+        self.titleText = "오늘은 난장판입니다!\n함께 즐겨요!"
+        self.iconOpacity = 1.0
+        self.startOpacityAnimation()
+      } else {
+        self.titleText = "오늘은 난장판이 아닙니다..\n다음 기회에.."
+        self.iconOpacity = 0.5
+      }
+    }.store(in: &setCancellable)
   }
   
   func fetchArcade() {
@@ -27,7 +39,9 @@ class MainViewModel: ObservableObject {
       
     }) { [weak self] arcade in
       guard let self = self else { return }
-      print(arcade)
+      let isTodayMayhem = arcade.getModes().contains { mode in
+        mode.id == 62
+      }
       self.mode1 = arcade.modes.tile_1
       self.mode2 = arcade.modes.tile_2
       self.mode3 = arcade.modes.tile_3
@@ -35,6 +49,14 @@ class MainViewModel: ObservableObject {
       self.mode5 = arcade.modes.tile_5
       self.mode6 = arcade.modes.tile_6
       self.mode7 = arcade.modes.tile_7
+
+      self.totalMayhemFilter.send(isTodayMayhem)
     }.store(in: &setCancellable)
+  }
+  
+  private func startOpacityAnimation() {
+    withAnimation(Animation.easeInOut(duration: 1).repeatForever()) {
+      self.iconOpacity = 0
+    }
   }
 }
