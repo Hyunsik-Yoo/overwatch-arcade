70b5d0866b4573f34f9c765686c653bdd6664c5e
hyunsik yoo <hyunsikyoo@seerslab.com>
2021-01-05 14:35:50 +0900

    메인화면 잔잔바리 그리는중

--- overwatch-arcade/features/main/MainView.swift
+++ overwatch-arcade/features/main/MainView.swift
@@ -22,9 +22,24 @@ class MainView: BaseView {
     $0.image = UIImage(named: "img_mayhem")
   }
   
+  let dateLabel = UILabel().then {
+    $0.text = "2020. 09.30"
+    $0.font = UIFont(name: "koverwatch", size: 20)
+    $0.textColor = .white
+  }
+  
+  let titleLabel = UILabel().then {
+    $0.numberOfLines = 0
+    $0.text = "오늘은 난장판이 아닙니다..\n다음기회에.."
+    $0.textColor = .white
+    $0.font = UIFont(name: "koverwatch", size: 36)
+  }
+  
   
   override func setup() {
-    addSubViews(backgroundImage, historyButton, mayhemImage)
+    containerView.addSubViews(mayhemImage, dateLabel, titleLabel)
+    scrollView.addSubview(containerView)
+    addSubViews(backgroundImage, historyButton, scrollView)
   }
   
   override func bindConstraints() {
@@ -38,10 +53,32 @@ class MainView: BaseView {
       make.width.height.equalTo(24)
     }
     
+    self.scrollView.snp.makeConstraints { make in
+      make.left.right.bottom.equalToSuperview()
+      make.top.equalTo(self.historyButton.snp.bottom).offset(10)
+    }
+    
+    self.containerView.snp.makeConstraints { make in
+      make.edges.equalTo(self.scrollView)
+      make.width.equalTo(UIScreen.main.bounds.width)
+      make.top.equalToSuperview()
+      make.bottom.equalTo(titleLabel.snp.bottom)
+    }
+    
     self.mayhemImage.snp.makeConstraints { make in
       make.left.equalToSuperview().offset(24)
       make.top.equalTo(self.historyButton.snp.bottom).offset(63)
       make.width.height.equalTo(108 * RatioUtils.widthRatio)
+    }
+    
+    self.dateLabel.snp.makeConstraints { make in
+      make.left.equalToSuperview().offset(24)
+      make.top.equalTo(self.mayhemImage.snp.bottom).offset(17)
+    }
+    
+    self.titleLabel.snp.makeConstraints { make in
+      make.left.equalToSuperview().offset(24)
+      make.top.equalTo(self.dateLabel.snp.bottom).offset(16)
     }
   }
 }
